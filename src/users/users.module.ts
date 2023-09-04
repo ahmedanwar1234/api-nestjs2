7c244773af107873f53d5f2731d6f902d54cf387
ahmedanwar123 <ahmedalprenses@gmail.com>
2023-09-04 09:31:21 +0300

    first commasdfat

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,12 +5,12 @@ import UsersController from "./user.controller";
 import { UserService } from "./user.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UserSchema } from "./user.models";
-import { PassportModule }from '@nestjs/passport'
-import { JwtModule }from '@nestjs/jwt'
-import { ConfigService }from '@nestjs/config'
-import { config } from "process";
-import { APP_GUARD } from "@nestjs/core";
-import { RolesGuard } from "./role.guard";
+// import { PassportModule }from '@nestjs/passport'
+// import { JwtModule }from '@nestjs/jwt'
+// import { ConfigService }from '@nestjs/config'
+// import { config } from "process";
+// import { APP_GUARD } from "@nestjs/core";
+// import { RolesGuard } from "./role.guard";
 @Module({
     
     imports:[MongooseModule.forFeature([{name:'testchema',schema:UserSchema}])],
